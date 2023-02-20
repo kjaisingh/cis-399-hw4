d51e6b7ed48d739dbbdd76506c135dd0845b77a5
Karan <kj.jaisingh@gmail.com>
2023-02-20 17:06:19 -0500

    First

--- image.js
+++ image.js
@@ -1,42 +1,18 @@
-const customName = document.getElementById('customname');
-const randomize = document.querySelector('.randomize');
-const story = document.querySelector('.story');
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
 
-function randomValueFromArray(array){
-  const random = Math.floor(Math.random()*array.length);
-  return array[random];
-}
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
 
-const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
-const insertX = [ "Willy the Goblin", "Big Daddy", "Father Christmas" ];
-const insertY = [ "the soup kitchen", "Disneyland", "the White House" ];
-const insertZ = [ "spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away" ];
+/* Declaring the array of image filenames */
 
-randomize.addEventListener('click', result);
+/* Declaring the alternative text for each image file */
 
-function result() {
-  let newStory = storyText;
+/* Looping through images */
 
-  const itemX = randomValueFromArray(insertX);
-  const itemY = randomValueFromArray(insertY);
-  const itemZ = randomValueFromArray(insertZ);
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
 
-  newStory = newStory.replaceAll(":insertx:", itemX);
-  newStory = newStory.replace(":inserty:", itemY);
-  newStory = newStory.replace(":insertz:", itemZ);
-
-  if(customName.value !== '') {
-    const name = customName.value;
-    newStory = newStory.replace("Bob", name);
-  }
-
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300/14) + " stone";
-    const temperature = Math.round(((94-32)*(5/9)) + " centigrade";
-    newStory = newStory.replace("300 pounds", weight);
-    newStory = newStory.replace("94 fahrenheit", temperature);
-  }
-
-  story.textContent = newStory;
-  story.style.visibility = 'visible';
-}
+/* Wiring up the Darken/Lighten button */
